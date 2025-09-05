e561d71a2cf9e840ac7c4ecdb0976d818cf46fd6
ravindra bankapur <rsbravindra@gmail.com>
2025-09-05 10:53:58 +0530

    Refactor: Rename RabbitMQConfig file

--- ems/src/main/java/com/test/ems/config/RabbitMQConfig.java
+++ ems/src/main/java/com/test/ems/config/RabbitMQConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RabitMQConfig {
+public class RabbitMQConfig {
     public static final String EXCHANGE = "employee.exchange";
     public static final String ROUTING_KEY = "employee.routing.key";
     public static final String QUEUE = "employee.notifications";
--- ems/src/main/java/com/test/ems/publisher/EmployeeEventPublisher.java
+++ ems/src/main/java/com/test/ems/publisher/EmployeeEventPublisher.java
@@ -1,6 +1,6 @@
 package com.test.ems.publisher;
 
-import com.test.ems.config.RabitMQConfig;
+import com.test.ems.config.RabbitMQConfig;
 import com.test.ems.dto.EmployeeEvent;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +12,7 @@ public class EmployeeEventPublisher {
     private RabbitTemplate rabbitTemplate;
 
     public void publishEmployeeUpdate(EmployeeEvent event) {
-        rabbitTemplate.convertAndSend(RabitMQConfig.EXCHANGE, RabitMQConfig.ROUTING_KEY,event);
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, RabbitMQConfig.ROUTING_KEY,event);
     }
 
 }
